071e5e12ab48e162bd4a1017402a2afd44572033
Tomoki Mori <mori-tomoki@g.ecc.u-tokyo.ac.jp>
2023-04-21 19:07:21 +0900

    linked list explanation

---
+++ list.c
@@ -0,0 +1,10 @@
+/*
+ * - advantage on using linked-list
+ * easily insert(and delete) value (when we insert value between indices of array,
+ * we do not need to move value to next one by one)
+ * - disadvantage
+ * hard to search n-th index value (too late(O(n))) because we have to chase linked list from head
+ * of the list to n-th index (list don't have index)
+ */
+
+int main() {}
